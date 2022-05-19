8cb7fbe0d17164138810360d4432d27c7d7eef7a
VaticUI <53976524+VaticUI@users.noreply.github.com>
2022-05-20 01:43:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const { autoUpdater } = require("electron-updater")
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 768,
+    icon: __dirname + '/index.ico',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+
+    
+  })
+  mainWindow.removeMenu()
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('src/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+// 1. Require the installed modul
+const sendStatusToWindow = (text) => {
+  log.info(text);
+  if (mainWindow) {
+    mainWindow.webContents.send('message', text);
+  }
+};
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', info => {
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', info => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', err => {
+  sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
+});
+autoUpdater.on('download-progress', progressObj => {
+  sendStatusToWindow(
+    `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
+  );
+});
+autoUpdater.on('update-downloaded', info => {
+  sendStatusToWindow('Update downloaded; will install now');
+});
+
+autoUpdater.on('update-downloaded', info => {
+  // Wait 5 seconds, then quit and install
+  // In your application, you don't need to wait 500 ms.
+  // You could call autoUpdater.quitAndInstall(); immediately
+  autoUpdater.quitAndInstall();
+});
---
+++ renderer.js
@@ -0,0 +1,13 @@
+var block1 = document.querySelector('.block1');
+var block2 = document.querySelector('.block2');
+var block3 = document.querySelector('.block3');
+
+block1.onclick = function(){
+    window.open('https://manager.adkynet.com/xlkdfjgiosldikfjg/login.php?redirect=%2Fxlkdfjgiosldikfjg%2F', '_blank', 'top=0,left=0,nodeIntegration=no,removeMenu=yes')
+}
+block2.onclick = function(){
+    window.open('https://panel.adkynet.com/', '_blank', 'top=0,left=0,nodeIntegration=no,removeMenu=yes')
+}
+block3.onclick = function(){
+    window.open('https://bot.adkynet.com/administration/dashboard', '_blank', 'top=0,left=0,nodeIntegration=no,setMenuBarVisibility=false')
+}
